eba9e14a59474be5287fd4e400b537843f1f4c29
Koshy Muthalaly <koshy.muthalaly88@gmail.com>
2019-12-20 19:38:06 +0530

    first commit

---
+++ Assets/Scenes/ScreenLoader.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class ScreenLoader : MonoBehaviour
+{
+
+    [SerializeField] int min;
+    [SerializeField] int max;
+    [SerializeField] TextMeshProUGUI guess;
+    int nextGuessNo=500;
+
+
+    public void nextGuess() {
+        
+        nextGuessNo = Random.Range(min, max);
+        Debug.Log(nextGuessNo + "::"+ min +" ::" +max);
+        guess.text = nextGuessNo.ToString();
+    
+    }
+
+    public void isHigher() {
+        Debug.Log("Calling is Higher"+nextGuessNo);
+        min = nextGuessNo + 1;
+        nextGuess();
+    }
+    public void isLower()
+    {
+        Debug.Log("Calling is Lower" + nextGuessNo);
+        max = nextGuessNo - 1;
+        nextGuess();
+    }
+
+    public void loadNextScene() {
+
+        int index = SceneManager.GetActiveScene().buildIndex;
+        Debug.Log(SceneManager.GetActiveScene().ToString());
+        SceneManager.LoadScene(index+1);
+    
+    }
+
+    public void loadStartScene() {
+
+        SceneManager.LoadScene(0);
+    
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Start() { 
+    
+        
+    }
+
+}
